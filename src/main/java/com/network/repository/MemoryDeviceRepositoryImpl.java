cf59f3f61cafaeefc801366c476a50d201fe4651
Konstantin <qwerty750@gmail.com>
2021-09-14 18:27:32 +0200

    MemoryDeviceRepositoryImpl -> get rid of lock

--- src/main/java/com/network/repository/MemoryDeviceRepositoryImpl.java
+++ src/main/java/com/network/repository/MemoryDeviceRepositoryImpl.java
@@ -4,25 +4,22 @@ import com.network.model.Device;
 import lombok.NonNull;
 import org.springframework.util.CollectionUtils;
 
-import java.util.*;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
+//todo implement storing and loading from file
 public class MemoryDeviceRepositoryImpl implements DeviceRepository{
-    private final ReadWriteLock readWriteLock;
-    private final Set<Device> devices;
     private final Map<String, Device> deviceMap;
 
     public MemoryDeviceRepositoryImpl() {
         this(null);
-        //todo implement loading from file
     }
 
     //for integration tests
     public MemoryDeviceRepositoryImpl(Collection<Device> initialData) {
-        this.readWriteLock = new ReentrantReadWriteLock();
-        this.devices = new HashSet<>();
-        this.deviceMap = new HashMap<>();
+        this.deviceMap = new ConcurrentHashMap<>();
         if (!CollectionUtils.isEmpty(initialData)) {
             initialData.forEach(this::addDevice);
         }
@@ -30,28 +27,16 @@ public class MemoryDeviceRepositoryImpl implements DeviceRepository{
 
     @Override
     public Device addDevice(@NonNull Device device) throws DeviceAlreadyExistsException {
-        readWriteLock.writeLock().lock();
-        try {
-            String macAddress = device.getMacAddress().toNormalizedString();
-            if (deviceMap.containsKey(macAddress)) {
-                throw new DeviceAlreadyExistsException(macAddress);
-            }
-            devices.add(device);
-            deviceMap.put(macAddress, device);
-            //todo implement storing in some file
-            return device;
-        } finally {
-            readWriteLock.writeLock().unlock();
+        String macAddress = device.getMacAddress().toNormalizedString();
+        if (deviceMap.containsKey(macAddress)) {
+            throw new DeviceAlreadyExistsException(macAddress);
         }
+        deviceMap.put(macAddress, device);
+        return device;
     }
 
     @Override
     public Set<Device> getAllDevices() {
-        readWriteLock.readLock().lock();
-        try {
-            return Set.copyOf(devices);
-        } finally {
-            readWriteLock.readLock().unlock();
-        }
+        return Set.copyOf(deviceMap.values());
     }
 }
